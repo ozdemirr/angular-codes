8e37ed62e9123fc17872fb194a173cff4b09d46a
emre <emre@gelistir.net>
2019-11-04 23:04:26 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'app';
-  ikininDegeri = '';
+  @ViewChild('iki', {static: true}) iki: ElementRef;
 
-  guncelle(iki: HTMLInputElement) {
-    this.ikininDegeri = iki.value;
+  bastir() {
+    alert(this.iki.nativeElement.value);
   }
+
 }
