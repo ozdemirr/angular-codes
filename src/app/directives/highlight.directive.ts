77fc3460bc8a93f73122d626c7db9fe9df0149e8
emre <emre@gelistir.net>
2019-11-09 20:14:05 +0300

    1

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,14 +1,18 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective implements OnInit {
+export class HighlightDirective {
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
-  ngOnInit() {
-    this.elementRef.nativeElement.style.backgroundColor = 'yellow';
+  @HostListener('mouseenter') mouseenter(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
   }
 
 }
