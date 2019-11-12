a4b1490e86c074f5fb3f92c70c5395aac1965d10
emre <emre@gelistir.net>
2019-11-12 23:28:44 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggingService } from './services/logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers : [LoggingService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private logging: LoggingService) {
+
+  }
+
+  ngOnInit() {
+    this.logging.log('component initiated');
+  }
 
 }
